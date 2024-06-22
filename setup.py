84f7c471f332ca424ad9f2a19fafba086747f00d
lalit <lalit.kumar@ponderbot-analytics.com>
2024-06-22 19:26:45 +0530

    Create setup.py
    create setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [reqmts.replace('\n', "") for reqmts in requirements]
+    return requirements
+
+
+setup(
+    name='gpife',
+    version=0.0.1,
+    author='lalit',
+    packages=find_packages(),
+    install_requires = get_requirements(r'requirements.txt'),
+    description='This is about generating the high quality product image from given text prompt for E-commerce'
+)
